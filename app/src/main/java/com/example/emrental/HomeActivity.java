3d546be9cd88ae34ce4f08d1dff0fafa0e76ebe2
Mohamad Amer <38558741+amere17@users.noreply.github.com>
2020-03-04 15:56:46 +0200

    Update HomeActivity.java

--- app/src/main/java/com/example/emrental/HomeActivity.java
+++ app/src/main/java/com/example/emrental/HomeActivity.java
@@ -1,15 +1,20 @@
+/*
+Written by:Mohamad Amer & Muhammed Egbaryia
+Date: 4/03/2020
+Subject: Sign In class for registered users
+*/
 package com.example.emrental;
-
+//---------------- Android imports ------------------------
 import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
-
 import com.google.firebase.auth.FirebaseAuth;
 
+//-------------------- Main Activity for users after login ------------------
 public class HomeActivity extends AppCompatActivity {
+    //------------- Variables & Objects -------------------
     FirebaseAuth fireA;
     private FirebaseAuth.AuthStateListener mAuthL;
     Button logoutBtn;
@@ -17,7 +22,9 @@ public class HomeActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+        //-------------- attach variables with XML file ----------
         logoutBtn = (Button)findViewById(R.id.button6);
+        //-------------- method for Logout Button --------------
         logoutBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
