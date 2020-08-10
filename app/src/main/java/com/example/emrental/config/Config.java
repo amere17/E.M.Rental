bc2e42e21902ccfdd6045d4d4056a8a3130e815c
Mohamad Amer <38558741+amere17@users.noreply.github.com>
2020-08-11 01:47:16 +0300

    Update Config.java

--- app/src/main/java/com/example/emrental/config/Config.java
+++ app/src/main/java/com/example/emrental/config/Config.java
@@ -1,5 +1,5 @@
 package com.example.emrental.config;
 //----------------- PayPal Id Client ---------------------
 public final class Config {
-    public static final String PAYPAL_CLIENT_ID = "AQJE1KLRV63nlnUK3AFXnb6viA_7YkTKnAZg5EeMgVlVn0csdJNca55Q2ppqVSEfsq1o238TuZGIuRBI";
+    public static final String PAYPAL_CLIENT_ID = "-------";
 }
